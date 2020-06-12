119cf0b5ebc502cc93b3f7f882c5bf4b58f30306
Enzo Daneri <enzodaneri@hotmail.com>
2020-06-11 22:53:25 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+import Github from './Github.svelte';
+</script>
+
+<Github/>
---
+++ src/Github.svelte
@@ -0,0 +1,70 @@
+<!-- <script>
+import { onMount } from 'svelte';
+
+let users = [];
+let loading = true;
+
+onMount(async () => {
+   let userData = await fetch('https://api.github.com/users');
+   let githubUsers = await userData.json();
+   users = githubUsers;
+   loading = false;   
+});
+
+</script>   
+
+<style>
+h2 {
+    text-align: center;
+}
+
+</style>
+
+{#if loading}
+ <h2>Loading...</h2>
+ {:else}
+ <section>
+ {#each users as user}
+ <article class="user">
+ <img src={user.avatar_url} alt={user.login}>
+ <div class="user-info">
+ <h3>User: {user.login}</h3>
+ <a href={user.html_url} class="btn-primary" target="blank">Github url</a>
+ </div>
+ </article>
+ {/each}
+ 
+ </section>
+{/if} -->
+
+<!-- /////////////////////////FORMA PROPIA DE SVELTE/////////////////////////////////////////// -->
+- <script>
+
+async function  getUsers() { 
+ let userData = await fetch('https://api.github.com/users');
+   let githubUsers = await userData.json();
+   return githubUsers;
+}
+
+</script>  
+
+<section>
+{#await getUsers()}
+<!-- promise is pending -->
+<h1>Loading</h1>
+{:then users}
+ {#each users as user}
+ <article class="user">
+ <img src={user.avatar_url} alt={user.login}>
+ <div class="user-info">
+ <h3>User: {user.login}</h3>
+ <a href={user.html_url} class="btn-primary" target="blank">Github url</a>
+ </div>
+ </article>
+ {/each}
+ {:catch error}
+ <!-- promise was rejected -->
+<p>Something went wrong : {error.message}</p>
+{/await}
+
+</section> 
